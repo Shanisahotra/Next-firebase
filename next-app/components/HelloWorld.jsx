6277cb47f69ca9cf755812e9f720552f4cfff337
Shanisahotra <jonathanzeeshan25@gmail.com>
2026-03-09 00:31:46 +0500

    created HelloWorld compt

---
+++ next-app/components/HelloWorld.jsx
@@ -0,0 +1,9 @@
+"use client"; // needed for client-side components
+
+export default function HelloWorld() {
+  return (
+    <div className="flex items-center justify-center h-64 bg-gray-100 text-xl font-bold text-blue-600">
+      Hello World
+    </div>
+  );
+}
